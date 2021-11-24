c149b06fa6be74d451aee59bef20736566dd4424
Parth <mittalparth22@gmail.com>
2021-11-24 22:51:57 +0530

    removed prev custom auth implementation

--- base/urls.py
+++ base/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, SignUpPage
-from django.contrib.auth.views import LogoutView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
-    path('login/',CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page = 'login'), name = 'logout'),
-    path('signup/', SignUpPage.as_view(), name = 'signup'),
     path('', TaskList.as_view(), name='tasks'),
     path('task-create', TaskCreate.as_view(), name='task-create'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'), #this name should match with context_object_name
--- base/views.py
+++ base/views.py
@@ -1,45 +1,15 @@
 from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 # Just to redirect from one page to another
 from django.urls import reverse_lazy 
 
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.forms import UserCreationForm
 # To enable a user to directly login after registration
 from django.contrib.auth import login
 from .models import Task
 # Create your views here.
-
-class CustomLoginView(LoginView):
-    template_name = 'base/login.html'
-    fields = '__all__'
-    redirect_authenticated_user = True # A user which is already authenticated cant access this page now
-
-    def get_success_url(self): # We are now defining a function for our success_url
-        return reverse_lazy('tasks')
-
-class SignUpPage(FormView):
-    template_name = 'base/signup.html'
-    form_class = UserCreationForm
-    # redirect_authenticated_user = True # This doesnt work for UserCreationForm
-    success_url = reverse_lazy('tasks')
-
-    #A function to handle data from the SignUp page
-    def form_valid(self, form):
-        user = form.save()
-        if user is not None:
-            login(self.request, user) # Automatically login
-        return super(SignUpPage, self).form_valid(form) # Otherwise, do what you were doing
-    
-    # We write a custom method to not allow a user to access the register page if logged in
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            return redirect('tasks')
-        return super(SignUpPage, self).get(*args, **kwargs)
-
 
 class TaskList(LoginRequiredMixin ,ListView):
     model = Task
